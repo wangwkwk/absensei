dcf0f43eb3606e80753a7a300de7128e96eb6f9a
wangwkwk <ihwantaufiq100@gmail.com>
2026-05-07 14:29:04 +0800

    fix

--- src/pages/each/success.tsx
+++ src/pages/each/success.tsx
@@ -4,17 +4,18 @@ import { titleChanger } from "../../components/libs/pageHead/pageHead"
 const Success = () => {
     titleChanger("Absen Berhasil")
     return (
-        <div className="flex w-screen h-screen items-center justify-center gap-10 p-4">
+        <div className="flex w-screen h-screen items-center justify-start flex-col  gap-10 p-10">
+            <h1 className="text-5xl text-secondary text-shadow-2xs mb-16"><strong>Absensei</strong></h1>
+
             <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-4xl font-bold text-secondary">
                     Berhasil hadir !!
                 </h1>
-                <div>
-                    <img
-                    src="/file/stiker.jpeg"
-                    />
-                </div>
-                </div>
+
+                <img
+                src="/file/stiker.jpeg"
+                />
+            </div>
         </div>
     )
 }
